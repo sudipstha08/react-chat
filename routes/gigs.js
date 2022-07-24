7d2c9c3777f3ecb97c41fab32f4f8b284f32670a
sudipstha08 <sudipstha088@gmail.com>
2022-07-24 12:44:28 +0545

    add layout

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,3 +1,4 @@
+const { application } = require('express')
 const express = require('express')
 const router = express.Router()
 const db = require('../config/database')
@@ -8,5 +9,31 @@ router.get('/', (req, res) => Gig.findAll()
    console.log(gigs)
    res.sendStatus(200)})
 .catch(err => console.log(err)))
+
+// HandleBars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', handlebars)
+
+router.get('/add', (req, res) => {
+   const data = {
+      title: 'Wordpres',
+      techs: "wordpress",
+      budget:"$2000",
+      description:"fsdfdsfdsddfsdf",
+      email:"rest2@gmail.com"
+   }
+
+   let {title, techs, email, description, budget} = data
+
+   // Insert into table
+   Gig.create({
+      title,
+      techs,
+      description,
+      budget,
+      email
+   }).then(gig => res.redirect('/gigs'))
+   .catch(err => console.log(err))
+})
 
 module.exports = router
